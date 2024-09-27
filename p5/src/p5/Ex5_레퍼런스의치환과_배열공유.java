708b4aff79ae2b47ee7485da1f44c58abac367ae
ymmkh5959@gmail.com <ymmkh5959@gmail.com>
2024-09-27 16:26:30 +0900

    1

--- p5/src/p5/Ex5_레퍼런스의치환과_배열공유.java
+++ p5/src/p5/Ex5_레퍼런스의치환과_배열공유.java
@@ -3,7 +3,29 @@ package p5;
 public class Ex5_레퍼런스의치환과_배열공유 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//ppt21
+		int ia[] = {10,20,30,40,50};
+		int myArray[] = ia;//레퍼런스의 치환
+		
+		System.out.println("원래ia---");
+		for(int i =0; i<ia.length ; i++)
+		{
+			System.out.printf("ia[%d] : %d\n",i,ia[i]);
+		}
+		
+		myArray[3] = 999;
+		
+		System.out.println("13처리후 ia---");
+		for(int i =0; i<ia.length ; i++)
+		{
+			System.out.printf("ia[%d] : %d\n",i,ia[i]);
+		}
+		
+		System.out.println("myArray---");
+		for(int i =0; i<myArray.length ; i++)
+		{
+			System.out.printf("myArray[%d] : %d\n",i,myArray[i]);
+		}
 
 	}
 
---
+++ p5/src/p5/Ex6_최대값.java
@@ -0,0 +1,10 @@
+package p5;
+
+public class Ex6_최대값 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
