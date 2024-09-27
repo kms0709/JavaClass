e8171855de205cd58173f8f0dc3ebe18a153bf1d
ymmkh5959@gmail.com <ymmkh5959@gmail.com>
2024-09-27 16:19:08 +0900

    1

--- p5/src/p5/Ex4_일차원배열.java
+++ p5/src/p5/Ex4_일차원배열.java
@@ -5,7 +5,36 @@ public class Ex4_일차원배열 {
 	public static void main(String[] args) {
 		int ia[]=new int[5];//정수형 배열 객체 생성 및 선언
 		double []da= {1.2, 2.3, 3.4};
-
+		String st[] = {"사과","바나나","오렌지","딸기"};
+		char ch[]; //레퍼런스의 선언
+		ch = new char[6];//char형의 배열객체 생성
+		ch[3] = '가';
+		System.out.println("ia배열요소의 개수 : " +ia.length); //5
+		System.out.println("ia배열요소의 개수 : " +da.length); //3
+		
+		System.out.println("\nia---");
+		for(int i =0; i<ia.length ; i++)
+		{
+			System.out.printf("ia[%d] : %d\n",i,ia[i]);
+		}
+		
+		System.out.println("\nda---");
+		for(int i = 0 ; i < da.length; i++)
+		{
+			System.out.printf("da[%d] : %.1f\n", i,da[i]);
+		}
+		
+		System.out.println("\nst---");
+		for(int i = 0 ; i < st.length; i++)
+		{
+			System.out.printf("st[%d] : %s\n", i,st[i]);
+		}
+		
+		System.out.println("\nch---");
+		for(int i = 0 ; i < ch.length; i++)
+		{
+			System.out.printf("ch[%d] : %c\n", i,ch[i]);
+		}
 	}
 
 }
---
+++ p5/src/p5/Ex5_레퍼런스의치환과_배열공유.java
@@ -0,0 +1,10 @@
+package p5;
+
+public class Ex5_레퍼런스의치환과_배열공유 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
