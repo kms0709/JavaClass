08200202da8403d8f89d76cc40250f6f61edcdef
ymmkh5959@gmail.com <ymmkh5959@gmail.com>
2024-10-04 17:30:59 +0900

    4교시 커밋

--- p6/src/p6/Panmae.java
+++ p6/src/p6/Panmae.java
@@ -1,5 +1,34 @@
 package p6;
 
-public class Panmae {
-
+public class Panmae
+{
+	private char code;
+	private String pname;
+	private int su;
+	private int dan;
+	private int price;
+	
+	public void calc()
+	{
+		this.price = this.su * this.dan; //price = su * dan;
+	}
+	
+	public void Show()
+	{
+		calc();
+		System.out.println("코드 : " + code);
+		System.out.println("품명 : " + pname);
+		System.out.println("수량 : " + this.su);
+		System.out.println("단가 : " + dan);
+		System.out.println("금액 : " + price);
+	}
+	
+	public Panmae(char code, String pname ,int su ,int dan)
+	{
+		//code = code; //매개변수 = 매개변수	
+		this.code = code; //멤버변수 = 매개변수
+		this.pname = pname;
+		this.su = su;
+		this.dan = dan;
+	}
 }
