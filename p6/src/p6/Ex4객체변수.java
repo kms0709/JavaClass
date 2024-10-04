46ab0cb6a2c1d3ba6ac43ff130da283d61336ee0
kms0709 <tkqmskzm@gmail.com>
2024-10-04 16:26:40 +0900

    3교시 중간커밋

--- p6/src/p6/Ex4객체변수.java
+++ p6/src/p6/Ex4객체변수.java
@@ -1,22 +1,30 @@
 package p6;
 
-public class Ex4객체변수 {
-
-	public static void main(String[] args) {
-		Sinsang p=new Sinsang();
-		p.setData("홍 길 동",20,75.5);
-      
-	}
-
-}
 class Sinsang{
 	private String name;
 	private int age;
 	private double weight;
+	
 	public Sinsang(){ System.out.println("생성자call~"); }
-	public void setData(String _name,int _age,double _weight){
-		name=_name;
-		age=_age;
-		weight=_weight;
+	public void setData(String _name,int _age,double _weight) {
+		name = _name;
+		age = _age;
+		weight = _weight;
+	}
+	
+	public void disp() {
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("체중 : " + weight);
+	}
+}
+
+public class Ex4객체변수 {
+
+	public static void main(String[] args) {
+		Sinsang p = new Sinsang();
+		p.setData("홍 길 동",20,75.5);
+		p.disp();
+		
 	}
 }
---
+++ p6/src/p6/Ex5생성자.java
@@ -0,0 +1,10 @@
+package p6;
+
+public class Ex5생성자 {
+
+	public static void main(String[] args) {
+		Sinsang p = new Sinsang("홍 길 동",20,75.5);
+
+	}
+
+}
