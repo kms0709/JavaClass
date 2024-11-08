8e70b298a42e2d7f71a3e48cea17a80d2017623c
ymmkh5959@gmail.com <ymmkh5959@gmail.com>
2024-11-08 15:07:44 +0900

    2교시 패키지생성 p11

---
+++ p11/src/p11/Sample.java
@@ -0,0 +1,18 @@
+package p11;
+
+import p11.Sample;
+
+public class Sample {
+	int id;
+	public Sample(int id) {this.id=id;}
+	public static void main(String[] args) {
+		Sample ob1=new Sample(3);
+		Sample ob2=new Sample(4);
+		Sample s;
+		
+		s=ob2;
+		ob1=ob2;
+		
+	}
+
+}
