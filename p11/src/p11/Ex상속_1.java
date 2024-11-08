30e15b13bbe091aaee8d3935e8e5bdeaf15e7f2d
이성현 <morgenrot8339@naver.com>
2024-11-08 16:50:29 +0900

    3교시 최종

--- p11/src/p11/Ex상속_1.java
+++ p11/src/p11/Ex상속_1.java
@@ -7,15 +7,21 @@ public class Ex상속_1 {
 
 	public static void main(String[] args) {
 		Circle p=new Circle(100,999,5);
-		System.out.println("중심점 : ["+p.getX()+" ,"+p.getY()+"]");
-		System.out.println("반지름 : "+p.getRadius());
-		System.out.println("원의 넓이 : "+p.getArea());
+		//System.out.println("중심점 : ["+p.getX()+" ,"+p.getY()+"]");
+		//System.out.println("반지름 : "+p.getRadius());
+		//System.out.println("원의 넓이 : "+p.getArea());
 	}
 
 }
 class Point{
-	
+	private int x;
+	private int y;
+	public Point(int x,int y){this.x=x; this.y=y;}
 }
 class Circle extends Point{
-	
+	private int radius;
+	public Circle(int x,int y,int radius) {
+		super(x,y);
+		this.radius=radius;
+	}
 }
