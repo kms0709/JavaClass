89fe3c79b02f014c72a86083cde9cbbaee03f8d6
이성현 <morgenrot8339@naver.com>
2024-11-29 17:47:09 +0900

    4교시 최종

--- p14/src/p14/Ex15_lotto.java
+++ p14/src/p14/Ex15_lotto.java
@@ -1,9 +1,14 @@
-package p14;
+package p14;/* 1<=	<=45
+당첨번호 :30 12 35 11 31 23
+		+행운번호 32
+
+Ex12 행운번호 당첨 활용하시면 됩니다.
+*/
 
 public class Ex15_lotto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
