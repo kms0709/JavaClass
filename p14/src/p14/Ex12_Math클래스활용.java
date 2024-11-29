8b353e84d6c07eeac864f7c52b58648752200366
이성현 <morgenrot8339@naver.com>
2024-11-29 17:46:52 +0900

    4교시 최종

--- p14/src/p14/Ex12_Math클래스활용.java
+++ p14/src/p14/Ex12_Math클래스활용.java
@@ -11,12 +11,12 @@ public class Ex12_Math클래스활용 {
 		System.out.println(Math.round(-3.14)); // -3
 		System.out.println(Math.round(-3.64)); // -4
 		System.out.println(Math.max(111, 999)); // 999
-		System.out.println(Math.min(111, 999)); // 111
-		// [1, 45] 사이의 정수형 난수 5개 발생
-		/*System.out.print("이번주 행운의 번호는 ");
+		System.out.println(Math.min(111, 999));// 111
+		//[1, 45] 사이의 정수형 난수 5개 발생
+		System.out.print("이번주 행운의 번호는 ");
 		for (int i=0; i<5; i++){
-			System.out.print((int)(Math.random()*45 + 1) + " "); // 난수 발생
-		}*/	
+			System.out.print((int)(Math.random()*(45-1 + 1) +1) + " "); // 난수 발생
+		}	
 
 	}
 
---
+++ p14/src/p14/Ex15_lotto.java
@@ -0,0 +1,10 @@
+package p14;
+
+public class Ex15_lotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
