71097528e9954e0cfd683a84d16013102a0666a0
kms0709 <tkqmskzm@gmail.com>
2024-11-29 14:24:55 +0900

    1교시 중간 커밋

--- p14/src/p14/Ex1_Wrapper객체.java
+++ p14/src/p14/Ex1_Wrapper객체.java
@@ -4,21 +4,27 @@ public class Ex1_Wrapper객체 {
 
 	public static void main(String[] args) {
 		// 기본 타입의 값으로 Wrapper 객체 생성
-		 //Integer i = Integer.valueOf(10);
-		 Integer i = 10; // OK.
-		 //Integer i = new Integer(10);
-		 Character c = Character.valueOf('c');
-		 Double f = Double.valueOf(3.14);
-		 Boolean b = Boolean.valueOf(true);
+		//Integer i = Integer.valueOf(10);
+		Integer i = 10; // OK. 자동으로 Integer 객체로 됨.(Boxing)
+		//Integer i = new Integer(10);
 		 
-		 System.out.println(i.getClass()); // class java.lang.Integer
-		 System.out.println(i.getClass().getName()); // java.lang.Integer
+		//Character c = Character.valueOf('c');
+		Character c = 'c';
+		
+		//Double f = Double.valueOf(3.14);
+		Double f = 3.14;
+		
+		//Boolean b = Boolean.valueOf(true);
+		Boolean b = true;
+		
+		System.out.println(i.getClass()); // class java.lang.Integer
+		System.out.println(i.getClass().getName()); // java.lang.Integer
 		 
-		 // 변수 출력
-		 System.out.println("i : " + i);
-		 System.out.println("c : " + c);
-		 System.out.println("f : " + f);
-		 System.out.println("b : " + b);
+		// 변수 출력
+		System.out.println("i : " + i);
+		System.out.println("c : " + c);
+		System.out.println("f : " + f);
+		System.out.println("b : " + b);
 	}
 
 }
